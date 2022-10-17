642ee62d642e90424296d492e2fc17f636977872
Adam Mawlawi <adam@mradam.us>
2022-10-16 19:50:42 -0500

    fetch post by id and finsh UI page

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,51 +1,57 @@
-import React from "react"
+import React, { useState, useEffect, useContext } from "react"
 import Edit from "../img/edit.png"
 import Delete from "../img/delete.png"
 import Menu from "./Menu"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
+import moment from "moment"
+import { AuthContext } from "../context/authContext"
+import axios from "axios"
 
 const Single = () => {
+  const [post, setPost] = useState({})
+  const location = useLocation()
+
+  const postId = location.pathname.split("/")[2]
+  const { currentUser } = useContext(AuthContext)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`http://localhost:8800/api/posts/${postId}`)
+        console.log(res.data)
+        setPost(res.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData()
+  }, [postId])
+  console.log(post)
   return (
     <div className='single'>
       <div className='content'>
-        <img
-          src='https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-          alt=''
-        />
+        <img src={post?.postImg} alt='' />
         <div className='user'>
           <img
             src='https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
             alt=''
           />
           <div className='info'>
-            <span>Adam</span>
-            <p>Posted 2 days ago</p>
+            <span>{post?.username}</span>
+            <p>Posted {moment(post.date).fromNow()}</p>
           </div>
-          <div className='edit'>
-            <Link to={`/write?edit=2`}>
-              <img src={Edit} alt='edit' />
-            </Link>
+          {currentUser?.username === post?.username && (
+            <div className='edit'>
+              <Link to={`/write?edit=2`}>
+                <img src={Edit} alt='edit' />
+              </Link>
 
-            <img src={Delete} alt='delete' />
-          </div>
+              <img src={Delete} alt='delete' />
+            </div>
+          )}
         </div>
-        <h1>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati
-          voluptatibus voluptatem qui deleniti nesc
-        </h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur,
-          magnam. Sit, eius voluptas. Distinctio quam soluta, provident
-          necessitatibus consequatur aperiam atque sequi. Ab alias, quod
-          corrupti animi possimus nostrum ducimus? Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Doloribus expedita, veniam repudiandae
-          labore sed voluptatem! Doloribus repellendus nulla aliquid hic dolore
-          nesciunt incidunt quia, at ad quod explicabo! Error, sunt. Lorem ipsum
-          dolor, sit amet consectetur adipisicing elit. Accusantium ex et nobis
-          quibusdam dignissimos aspernatur corporis obcaecati praesentium
-          doloribus voluptas impedit rem commodi voluptatem, itaque cum ea
-          maiores cumque asperiores.
-        </p>
+        <h1>{post?.title}</h1>
+        {post?.desc}
       </div>
       <div className='menu'>
         <Menu />

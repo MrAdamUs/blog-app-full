a2ee82b2c64e0f627ac3fc163624cf89d1f7aaa0
Adam Mawlawi <adam@mradam.us>
2022-10-18 22:05:18 -0500

    working on uplade img

--- api/index.js
+++ api/index.js
@@ -1,14 +1,21 @@
-import express from "express"
+import express, { json } from "express"
 import postRoutes from "./routes/posts.js"
 import authRoutes from "./routes/auth.js"
 import userRoutes from "./routes/users.js"
 import cors from "cors"
+import multer from "multer"
 import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors("Access-Control-Allow-Origin: *"))
 app.use(express.json())
 app.use(cookieParser())
+
+const upload = multer({ dest: "./uploads/" })
+
+app.post("/api/upload", upload.single("file"), function (req, res) {
+  res.status(200).json("Image has been uploaded")
+})
 
 app.use("/api/posts", postRoutes)
 app.use("/api/auth", authRoutes)
--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,14 +1,37 @@
 import React, { useState } from "react"
 import ReactQuill from "react-quill"
+import axios from "axios"
 import "react-quill/dist/quill.snow.css"
 
 const Write = () => {
   const [value, setValue] = useState("")
-  console.log(value)
+  const [title, setTitle] = useState("")
+  const [file, setFile] = useState(null)
+  const [cat, setCat] = useState("")
+
+  const upload = async () => {
+    try {
+      const formData = new FormData()
+      formData.append("file", file)
+      const res = await axios.post(`http://localhost:8800/api/upload`, formData)
+      console.log(res.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleClick = async (e) => {
+    e.preventDefault()
+    upload()
+  }
   return (
     <div className='add'>
       <div className='content'>
-        <input type={"text"} placeholder='title' />
+        <input
+          type={"text"}
+          placeholder='title'
+          onChange={(e) => setTitle(e.target.value)}
+        />
         <div className='editorContainer'>
           <ReactQuill
             className='editor'
@@ -27,40 +50,82 @@ const Write = () => {
           <span>
             <b>Visibility: </b> Public
           </span>
-          <input style={{ display: "none" }} type='file' id='file' name='' />
+          <input
+            style={{ display: "none" }}
+            type='file'
+            id='file'
+            name=''
+            onChange={(e) => setFile(e.target.files[0])}
+          />
           <label className='file' htmlFor='file'>
             Upload Image
           </label>
           <div className='buttons'>
             <button>Save as a draft</button>
-            <button>Update</button>
+            <button onClick={handleClick}>Publish</button>
           </div>
         </div>
         <div className='item'>
           <h1>Category</h1>
           <div className='cat'>
-            <input type='radio' name='cat' value='art' id='art' />
+            <input
+              type='radio'
+              name='cat'
+              value='art'
+              id='art'
+              onChange={(e) => setCat(e.target.value)}
+            />
             <label htmlFor='art'>Art</label>
           </div>
 
           <div className='cat'>
-            <input type='radio' name='cat' value='science' id='science' />
+            <input
+              type='radio'
+              name='cat'
+              value='science'
+              id='science'
+              onChange={(e) => setCat(e.target.value)}
+            />
             <label htmlFor='science'>Science</label>
           </div>
           <div className='cat'>
-            <input type='radio' name='cat' value='technology' id='technology' />
+            <input
+              type='radio'
+              name='cat'
+              value='technology'
+              id='technology'
+              onChange={(e) => setCat(e.target.value)}
+            />
             <label htmlFor='technology'>Technology</label>
           </div>
           <div className='cat'>
-            <input type='radio' name='cat' value='cinema' id='cinema' />
+            <input
+              type='radio'
+              name='cat'
+              value='cinema'
+              id='cinema'
+              onChange={(e) => setCat(e.target.value)}
+            />
             <label htmlFor='cinema'>Cinema</label>
           </div>
           <div className='cat'>
-            <input type='radio' name='cat' value='design' id='design' />
+            <input
+              type='radio'
+              name='cat'
+              value='design'
+              id='design'
+              onChange={(e) => setCat(e.target.value)}
+            />
             <label htmlFor='design'>Design</label>
           </div>
           <div className='cat'>
-            <input type='radio' name='cat' value='food' id='food' />
+            <input
+              type='radio'
+              name='cat'
+              value='food'
+              id='food'
+              onChange={(e) => setCat(e.target.value)}
+            />
             <label htmlFor='food'>Food</label>
           </div>
         </div>
